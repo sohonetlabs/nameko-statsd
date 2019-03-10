bfa6b5ec2c899bdaff7c7686ee49903548ced215
Julio Trigo <julio.trigo@gmail.com>
2019-03-10 13:27:00 +0000

    Amend license setup attribute

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'dev': reqs('requirements/dev.txt'),
     },
     zip_safe=True,
-    license='MIT',
+    license='MIT License',
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
